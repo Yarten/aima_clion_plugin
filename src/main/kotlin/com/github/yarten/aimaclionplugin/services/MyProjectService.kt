526e59eaaed78fc86e43c953c175eb3e16046332
GitHub Action <action@github.com>
2025-09-29 11:32:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/yarten/aimaclionplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/yarten/aimaclionplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.yarten.aimaclionplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yarten.aimaclionplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/yarten/aimaclionplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/yarten/aimaclionplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.yarten.aimaclionplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
